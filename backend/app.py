098fdd723aee6eed9a36ffafb2a449f31775102b
Robin Marques <robin1.marques@gmail.com>
2026-01-23 13:32:34 -0500

    Ajout d'une TABLE pour ma base de donnée et premier test et configuration de cette dernière

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,49 @@
-print("Le conteneur Python est opérationnel.")
+# ========================================
+# Auteur: Robin Marques
+# Langage: Python 3.11
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# =========================================
+
+# ===== CREATION DE MA BASE DE DONNEES =====
+
+import psycopg2
+import os
+
+# Paramètre pour la connexion à la base de données PostgreSQL
+BD_NAME = os.getenv("POSTGRES_DB", "ruche_db")
+BD_USER = os.getenv("POSTGRES_USER", "apiculteur")
+BD_PASSWORD = os.getenv("POSTGRES_PASSWORD", "password_pfe")
+BD_HOST = os.getenv("POSTGRES_HOST", "database")
+
+sql_create_table = """
+CREATE TABLE IF NOT EXISTS mesures_ruche (
+    id SERIAL PRIMARY KEY,
+    date_heure TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    temperature NUMERIC(5,2),
+    poids NUMERIC(6,3)
+);
+"""
+
+try:
+    with psycopg2.connect(
+        dbname=BD_NAME, 
+        user=BD_USER, 
+        password=BD_PASSWORD, 
+        host=BD_HOST
+    ) as conn:
+        with conn.cursor() as cur:
+            cur.execute(sql_create_table)
+
+except psycopg2.OperationalError:
+    print("Erreur de connexion à la base de données : {e}")
+else:
+    print("Connexion à la base de données réussie et création de la table.")
+finally:
+    print("Fin de la procédure de connexion.")
+
